97604197b94b8f1c8e1707face4571e8c1fcdcc5
Kazu (Phu Phan) <phankyphu@gmail.com>
2024-05-06 13:27:46 +0700

    Initial Commit

---
+++ PhuPhanNote/PhuPhanNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhuPhanNoteApp.swift
+//  PhuPhanNote
+//
+//  Created by Kazu on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct PhuPhanNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
